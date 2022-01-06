78d2015e1bfe2647cb884809157bc1aba15c0af2
yang <yangbo@live.cn>
2022-01-06 14:21:52 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -24,11 +24,13 @@ yarn build
 # 进入生成的构建文件夹
 cd ./dist
 
-# 如果你是要部署到自定义域名
-# echo 'www.example.com' > CNAME
+# 将静态文件提交
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你想要部署到 https://<USERNAME>.github.io
+# 静态文件push到github
 git push -f git@github.com:yangb0/yangb0.github.io.git master
+
+# 静态文件push到gitee
+git push -f git@gitee.com:dsnk/dsnk.git master
